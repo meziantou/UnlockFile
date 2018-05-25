664155e69fabf067355e9deb8c26dcc9e6ed7ba9
meziantou <meziantou@users.noreply.github.com>
2018-05-24 21:41:36 -0400

    Initial commit

--- UnlockFile/Program.cs
+++ UnlockFile/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Meziantou.Framework.Win32;
 
 namespace UnlockFile
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var paths = args.Where(IsPath).ToArray();
+            if (paths.Length == 0)
+            {
+                PrintUsage();
+                return 1;
+            }
+
+            using (var session = RestartManager.CreateSession())
+            {
+                session.RegisterFiles(paths);
+                var processes = session.GetProcessesLockingResources();
+                if (processes.Count == 0)
+                {
+                    Console.WriteLine("Not locked by a process");
+                    return 0;
+                }
+
+                foreach (var process in processes)
+                {
+                    Console.WriteLine($"{process.ProcessName} ({process.Id})");
+                }
+
+                Console.WriteLine("Do you want to close the processes? [y/N]");
+                var key = Console.ReadKey();
+                if (char.ToLowerInvariant(key.KeyChar) == 'y')
+                {
+                    session.Shutdown(RmShutdownType.RmForceShutdown);
+                    Console.WriteLine("Do you want to restart the processes? [y/N]");
+                    key = Console.ReadKey();
+                    if (char.ToLowerInvariant(key.KeyChar) == 'y')
+                    {
+                        session.Restart();
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        private static bool IsPath(string path)
+        {
+            try
+            {
+                return File.Exists(path) || Directory.Exists(path);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage: UnlockFile [path] [path] ...");
         }
     }
 }
